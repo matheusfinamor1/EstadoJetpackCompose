54d5346f72ffd089874a7b1533f43ac47fd05e2a
Matheus Finamor Correa <matheusfinamor1@gmail.com>
2023-06-12 13:40:45 -0300

    Eventos no Compose

--- app/src/main/java/com/example/basicsstatecodelab/WaterCounter.kt
+++ app/src/main/java/com/example/basicsstatecodelab/WaterCounter.kt
@@ -1,16 +1,25 @@
 package com.example.basicsstatecodelab
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun WaterCounter(modifier: Modifier = Modifier){
-    val count = 0
-    Text(
-        text = "You've had $count glasses.",
-        modifier = Modifier.padding(16.dp)
-    )
+fun WaterCounter(modifier: Modifier = Modifier) {
+    Column(modifier = Modifier.padding(16.dp)) {
+        var count = 0
+        Text(
+            text = "You've had $count glasses.",
+        )
+        Button(
+            onClick = { count++ },
+            Modifier.padding(top = 8.dp)
+        ) {
+            Text(text = "Add one")
+        }
+    }
 }
