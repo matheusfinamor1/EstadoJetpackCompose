c803b2e71a772994b0da6c203771f4a38109576c
Matheus Finamor Correa <matheusfinamor1@gmail.com>
2023-06-12 14:03:29 -0300

    Memoria (remember) em uma função de composição

--- app/src/main/java/com/example/basicsstatecodelab/WaterCounter.kt
+++ app/src/main/java/com/example/basicsstatecodelab/WaterCounter.kt
@@ -5,13 +5,39 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
+    /**
+     * Composição: Uma descrição da IU criada pelo Jetpack Compose ao executar elementos combinaveis.
+     *
+     * Composição Inicial: A criação de uma composição ao executar elementos combinaveis pela primeira
+     * vez.
+     *
+     * Recomposição: Nova execução de elementos combinaveis para atualizar a composição quando os
+     * dados mudam.
+     */
     Column(modifier = Modifier.padding(16.dp)) {
-        var count = 0
+        /**
+         * MutableState e State tornam o estado observavel (por conta do "value").
+         * O Compose monitora cada elemento combinavel que le propriedades do estado "value" e aciona
+         * uma recomposição quando "value" muda.
+         *
+         * Remember é utilizado como mecanismo para armazenar um unico objeto na composição.
+         * No caso, quando haver a recomposição, o valor do "count" não será perdido.
+         *
+         * Neste exemplo, estamos utilizando o delegate "by", que faz com que nao precise referenciar
+         * de forma explicita o ".value" do objeto.
+         */
+        var count by remember {
+            mutableStateOf(0)
+        }
         Text(
             text = "You've had $count glasses.",
         )
