a101db7ebf7a9a999606530ef61cd8929152f18e
Matheus Finamor Correa <matheusfinamor1@gmail.com>
2023-06-12 17:03:41 -0300

    MutableList observavel (toMutableStateList)

--- app/src/main/java/com/example/basicsstatecodelab/WellnessScreen.kt
+++ app/src/main/java/com/example/basicsstatecodelab/WellnessScreen.kt
@@ -2,12 +2,18 @@ package com.example.basicsstatecodelab
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
 
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier){
     Column {
         StatefulCounter(modifier)
-        WellnessTaskList()
+
+        val list = remember { getWellnessTasks().toMutableStateList() }
+        WellnessTaskList(list, onCloseTask = {task -> list.remove(task)})
     }
 }
+
+private fun getWellnessTasks() = List(30) {i -> WellnessTask(i, "Task # $i")}
--- app/src/main/java/com/example/basicsstatecodelab/WellnessTaskItem.kt
+++ app/src/main/java/com/example/basicsstatecodelab/WellnessTaskItem.kt
@@ -9,16 +9,13 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.basicsstatecodelab.ui.theme.BasicsStateCodelabTheme
 
 @Composable
 fun WellnessTaskItem(
@@ -51,7 +48,11 @@ fun WellnessTaskItem(
 }
 
 @Composable
-fun WellnessTaskItem(taskName: String, modifier: Modifier = Modifier) {
+fun WellnessTaskItem(
+    taskName: String,
+    onClose: () -> Unit,
+    modifier: Modifier = Modifier
+) {
     var checkedState by rememberSaveable {
         mutableStateOf(false)
     }
@@ -60,7 +61,7 @@ fun WellnessTaskItem(taskName: String, modifier: Modifier = Modifier) {
         taskName = taskName,
         checked = checkedState,
         onCheckedChange = { newValue -> checkedState = newValue },
-        onClose = { },
+        onClose = onClose,
         modifier = modifier
     )
 }
