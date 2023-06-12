2c8477ed675b0a8f8e8871b91eb6cb547c9a948c
Matheus Finamor Correa <matheusfinamor1@gmail.com>
2023-06-12 15:54:49 -0300

    Elevação de estado

--- app/src/main/java/com/example/basicsstatecodelab/WaterCounter.kt
+++ app/src/main/java/com/example/basicsstatecodelab/WaterCounter.kt
@@ -1,19 +1,18 @@
 package com.example.basicsstatecodelab
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
+/*
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     /**
@@ -57,4 +56,31 @@ fun WaterCounter(modifier: Modifier = Modifier) {
             Text(text = "Add one")
         }
     }
+}*/
+
+@Composable
+fun StatelessCounter(count: Int, onIncrement: () -> Unit, modifier: Modifier = Modifier){
+    Column(
+        modifier = modifier.padding(16.dp)
+    ) {
+        if(count > 0){
+            Text(text = "You've had $count glasses.")
+        }
+        Button(
+            onClick = onIncrement, Modifier.padding(top = 8.dp), enabled = count < 10
+        ) {
+            Text(text = "Add one")
+        }
+    }
 }
+
+@Composable
+fun StatefulCounter(modifier: Modifier = Modifier){
+    var count by rememberSaveable {
+        mutableStateOf(0)
+    }
+    StatelessCounter(count = count, onIncrement = {count++}, modifier)
+}
+
+
+
